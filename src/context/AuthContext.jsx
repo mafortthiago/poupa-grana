59844d86f582bebbd368af4164db3960d2b2502d
mafortthiago <thmafort@gmail.com>
2024-01-29 09:51:33 -0300

    feat: Add AuthContext

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,10 @@
+import { createContext, useContext } from "react";
+
+const AuthContext = createContext();
+
+export function AuthContextProvider({ children, value }) {
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+export function useAuthValue() {
+  return useContext(AuthContext);
+}
