01f7f0eb8d4a1e957d15bc0fa49e7b0063c56824
mafortthiago <thmafort@gmail.com>
2024-02-07 14:45:33 -0300

    feat: Add dinamic data in the panel and the money value

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles/pages/Home.scss";
 import imgEntry from "../../assets/img/entry.png";
 import imgExit from "../../assets/img/exit.png";
 import { AssetChart } from "../../components/AssetChart/AssetChart";
+import useItems from "../../hooks/useItems";
 export const Home = () => {
+  const { fetchItems } = useItems();
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const getItems = async () => {
+      const data = await fetchItems();
+      setItems(data);
+    };
+
+    getItems();
+  });
+
+  const soma = () => {
+    let value = 0;
+    items.map((item) => {
+      value += item.value;
+    });
+    return value;
+  };
   return (
     <main className="home">
       <section className="money-value">
         <h3>Dinheiro guardado:</h3>
-        <span>R$: 18,08</span>
-        <p>Opaaa! Parabéns seu patrimônio vem aumentando bastante!</p>
+        <span>R$: {soma()}</span>
+        {(() => {
+          if (soma() < -1000) {
+            return <p>Tome cuidado, tente economizar mais!</p>;
+          } else if (soma() < 0) {
+            return (
+              <p>
+                Você anda economizando pouco, mas temos certeza que vai dar a
+                volta por cima!
+              </p>
+            );
+          } else if (soma() === 0 && items.length !== 0) {
+            return (
+              <p>
+                Valor zerado, mas não se preocupe logo estará poupando bastante
+                dinheiro!
+              </p>
+            );
+          } else if (soma() < 1000) {
+            return <p>Você está no caminho certo, continue economizando</p>;
+          } else if (soma() < 10000) {
+            return (
+              <p>Você já está poupando um bom dinheiro, continue assim!</p>
+            );
+          } else {
+            return <p>Você é muito bom em guardar dinheiro, parabéns!!</p>;
+          }
+        })()}
       </section>
       <AssetChart />
       <section className="analyst">
@@ -21,24 +67,34 @@ export const Home = () => {
               <img src={imgEntry} />
               últimos ganhos:
             </h5>
-            <p>
-              R${15} - dia {"09/09/2021"}
-            </p>
-            <p>
-              R${150} - dia {"09/09/2021"}
-            </p>
+            {items &&
+              items.reduce((acc, item) => {
+                if (item.value > 0 && acc.length < 2) {
+                  acc.push(
+                    <p key={item.id}>
+                      R${item.value} - dia {item.created_at}
+                    </p>
+                  );
+                }
+                return acc;
+              }, [])}
           </div>
           <div className="losses">
             <h5>
               <img src={imgExit} alt="" className="losses_img" />
               últimas perdas:
             </h5>
-            <p>
-              R${17} - dia {"09/09/2021"}
-            </p>
-            <p>
-              R${25} - dia {"09/09/2021"}
-            </p>
+            {items &&
+              items.reduce((acc, item) => {
+                if (item.value < 0 && acc.length < 2) {
+                  acc.push(
+                    <p key={item.id}>
+                      R${-item.value} - dia {item.created_at}
+                    </p>
+                  );
+                }
+                return acc;
+              }, [])}
           </div>
         </div>
       </section>
